bb66b2eba87ccdbce6a1010370273e7042fa9e4c
AnthonySEO <32614421+Anthony-G24@users.noreply.github.com>
2021-03-06 16:34:42 +0000

    Update mastertickets.py

--- mastertickets.py
+++ mastertickets.py
@@ -1,8 +1,16 @@
-# Ticket system that shows number of tickets remaining, how much total order is and decrements num of tickets remaining once user confirms choice.
+# Ticket system that shows number of tickets remaining, how much total order value is and decrements num of tickets remaining once user confirms choice.
+# Uses functions, while loop, error handling and if statement
+
 
 TICKET_PRICE = 10
+SERVICE_CHARGE = 2
 
 tickets_remaining = 100  
+
+
+
+def calculate_price(number_of_tickets):
+	return (number_of_tickets * TICKET_PRICE) + SERVICE_CHARGE
 
 while tickets_remaining >= 1:
 	print("There are {} tickets remaining.".format(tickets_remaining))
@@ -15,7 +23,7 @@ while tickets_remaining >= 1:
 	except ValueError as err:
 		print("Sorry we ran into an issue. {}. Please try again.".format(err))
 	else:
-		total_price = number_of_tickets * TICKET_PRICE
+		total_price = calculate_price(number_of_tickets)
 		print("Your total comes to ${}".format(total_price))
 		decision = input("Do you want to proceed? Y/N  ")
 		if decision.lower() == "y":
